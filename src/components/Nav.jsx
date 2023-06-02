778d313a997a52ed37bbcf0ebafcb97f510cae6d
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-06-02 12:34:00 +0200

    add assets

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { React, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaRegTimesCircle, FaGripLines } from 'react-icons/fa';
 
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+    const [isOpen, setIsOpen] = useState(false);
 
-export default Nav
+    const toggleNav = () => {
+      setIsOpen(!isOpen);
+    };
+    return (
+      <>
+      <div className="mobile-nav" onClick={toggleNav}>
+        {isOpen ? <FaRegTimesCircle /> : <FaGripLines />}
+      </div>
+      <nav className={`nav ${isOpen ? 'active' : ''}`} onClick={toggleNav}>
+        <Link to="/" className="nav-item">HOME</Link>
+        <Link to="#" className="nav-item">ABOUT</Link>
+        <Link to="#" className="nav-item">MENU</Link>
+        <Link to="/booking" className="nav-item">RESERVATIONS</Link>
+        <Link to="#" className="nav-item">ORDER ONLINE</Link>
+        <Link to="#" className="nav-item">LOGIN</Link>
+      </nav>
+      </>
+    )
+  }
+
+  export default Nav
