33358838926f3438679c204f8d2e7f9f596cec26
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-06-02 19:30:04 +0200

    add variables.css to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import './App.css';
+import './variables.css';
 
 const App = () => {
   return (
