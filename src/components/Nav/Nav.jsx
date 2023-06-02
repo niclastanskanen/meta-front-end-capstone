5156f423df7a69ec0722e8ade0abdcf27da63cf4
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-06-03 01:02:36 +0200

    fix Logo link to go back to home when pressed

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logo from '../../assets/Logo.svg';
 import './Nav.css';
@@ -6,12 +7,14 @@ import './Nav.css';
 const Nav = () => {
   return (
     <nav className='nav'>
+    <Link to="/">
       <img src={logo} alt="Little Lemon Logo" className='logo' />
+      </Link>
       <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/menu">Menu</a></li>
-        <li><a href="/reservations">Reservations</a></li>
+        <li><a href="/booking">Reservations</a></li>
         <li><a href="/order">Order Online</a></li>
         <li><a href="/login">Login</a></li>
       </ul>
