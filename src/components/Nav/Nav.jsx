e589b7b6bcc7c022b969756cba87f33ba7fa0221
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-06-02 23:32:23 +0200

    change Nav component and split the css file

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import logo from '../../assets/Logo.svg';
+
+const Nav = () => {
+  return (
+    <nav className='nav'>
+      <img src={logo} alt="Little Lemon Logo" className='logo' />
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/menu">Menu</a></li>
+        <li><a href="/reservations">Reservations</a></li>
+        <li><a href="/order">Order Online</a></li>
+        <li><a href="/login">Login</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
