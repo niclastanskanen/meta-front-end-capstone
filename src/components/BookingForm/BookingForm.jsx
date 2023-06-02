ac8abeff7e2f44ead131b8d63089873ac37ce0d3
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-06-03 00:57:09 +0200

    fix booking page styling

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './BookingForm.css'
+
 const BookingForm = () => {
   return (
     <section aria-label="Booking Section" className="bookingSection">
@@ -7,20 +9,20 @@ const BookingForm = () => {
       <form aria-label="Booking Reservation Form" className="bookingForm">
         <div>
           <div aria-label="First Name Container" className="container firstNameContainer">
-            <label for="firstName">First Name<span aria-label="Required" className="required">*</span></label>
-            <input name="firstName" aria-label="First Name Input" aria-required="true" id="firstName" autocomplete="off" />
+            <label htmlFor="firstName">First Name<span aria-label="Required" className="required">*</span></label>
+            <input name="firstName" aria-label="First Name Input" aria-required="true" id="firstName" autoComplete="off" />
           </div>
           <div aria-label="Last Name Container" className="container lastNameContainer">
-            <label for="lastName">Last Name<span aria-label="Required" className="required">*</span></label>
-            <input name="lastName" aria-label="Last Name Input" aria-required="true" id="lastName" autocomplete="off" />
+            <label htmlFor="lastName">Last Name<span aria-label="Required" className="required">*</span></label>
+            <input name="lastName" aria-label="Last Name Input" aria-required="true" id="lastName" autoComplete="off" />
           </div>
         </div>
           <div aria-label="Date Container" className="container dateContainer">
-            <label for="date">Choose Date<span aria-label="Required" className="required">*</span></label>
+            <label htmlFor="date">Choose Date<span aria-label="Required" className="required">*</span></label>
             <input name="date" aria-label="Date Input" aria-required="true" id="date" type="date" />
           </div>
           <div aria-label="Time Container" className="container timeContainer">
-            <label for="time">Choose Time<span aria-label="Required" className="required">*</span></label>
+            <label htmlFor="time">Choose Time<span aria-label="Required" className="required">*</span></label>
             <select name="time" aria-label="Select Time" aria-required="true" id="time">
               <option>17:00</option>
               <option>17:30</option>
@@ -33,11 +35,11 @@ const BookingForm = () => {
             </select>
           </div>
             <div aria-label="Guest Container" className="container guestsContainer">
-            <label for="guests">Number of Guests<span aria-label="Required" className="required">*</span></label>
+            <label htmlFor="guests">Number of Guests<span aria-label="Required" className="required">*</span></label>
             <input name="guests" aria-label="Guests Input" aria-required="true" id="guests" type="number" min="0" placeholder="1" />
           </div>
           <div aria-label="Occasion Container" className="container occasionContainer">
-            <label for="occasion">Occasion</label>
+            <label htmlFor="occasion">Occasion</label>
             <select name="occasion" aria-label="Select Occasion" id="occasion">
               <option>None</option>
               <option>Birthday</option>
@@ -45,7 +47,7 @@ const BookingForm = () => {
               <option>Anniversary</option>
             </select>
           </div>
-            <button className="primaryBtn" text="Create Reservation" type="submit" style="border: 1px solid rgb(33, 33, 33);">
+            <button className="primaryBtn" text="Create Reservation" type="submit">
             Create Reservation
             </button>
       </form>
--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import BookingForm from '../components/BookingForm/BookingForm'
+
 const BookingPage = () => {
   return (
-    <div>BookingPage</div>
+    <BookingForm />
   )
 }
 
