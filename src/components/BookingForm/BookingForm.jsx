df389412dd698163485547cb10982e2f4182c386
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-06-05 09:14:33 +0200

    fix booking form and bookingpage to handle props to confirmedbooking page

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -2,40 +2,32 @@ import { useState } from 'react'
 
 import './BookingForm.css'
 
-const BookingForm = ({ availableTimes, dispatch, submitForm }) => {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    date: '',
-    time: '',
-    guests: '',
-    occasion: ''
-  })
-
-  const handleChange = (e) => {
-    const { name, value } = e.target
+const BookingForm = ({ availableTimes, dispatch, submitForm, formData, setFormData }) => {
+  const handleDateChange = async (event) => {
+    const { name, value } = event.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value
-    }))
+    }));
+    dispatch({ type: 'UPDATE_TIMES', payload: value });
   }
 
-  const handleDateChange = async (event) => {
-    const { name, value } = event.target
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value
-    }))
-    dispatch({ type: 'UPDATE_TIMES', payload: value })
+    }));
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    submitForm(formData)
+    console.log('Form Data', formData);
+    submitForm(formData);
   }
 
-  const currentDate = new Date().toISOString().split('T')[0]
-  const options = availableTimes.map(time => <option key={time}>{time}</option>)
+  const currentDate = new Date().toISOString().split('T')[0];
+  const options = availableTimes.map(time => <option key={time}>{time}</option>);
 
   return (
     <section aria-label="Booking Section" className="bookingSection">
@@ -44,42 +36,60 @@ const BookingForm = ({ availableTimes, dispatch, submitForm }) => {
         <div>
           <div aria-label="First Name Container" className="container firstNameContainer">
             <label htmlFor="firstName">First Name<span aria-label="Required" className="required">*</span></label>
-            <input name="firstName" aria-label="First Name Input" aria-required="true" id="firstName" autoComplete="off" onChange={handleChange} required />
+            <input
+              name="firstName"
+              aria-label="First Name Input"
+              aria-required="true"
+              id="firstName"
+              autoComplete="off"
+              onChange={handleInputChange}
+              value={formData.firstName}
+              required
+            />
           </div>
           <div aria-label="Last Name Container" className="container lastNameContainer">
             <label htmlFor="lastName">Last Name<span aria-label="Required" className="required">*</span></label>
-            <input name="lastName" aria-label="Last Name Input" aria-required="true" id="lastName" autoComplete="off" onChange={handleChange} required />
+            <input
+              name="lastName"
+              aria-label="Last Name Input"
+              aria-required="true"
+              id="lastName"
+              autoComplete="off"
+              onChange={handleInputChange}
+              value={formData.lastName}
+              required
+            />
           </div>
         </div>
-          <div aria-label="Date Container" className="container dateContainer">
-            <label htmlFor="date">Choose Date<span aria-label="Required" className="required">*</span></label>
-            <input name="date" aria-label="Date Input" aria-required="true" id="date" type="date" value={formData.date} onChange={handleDateChange} min={currentDate} required />
-          </div>
-          <div aria-label="Time Container" className="container timeContainer">
-            <label htmlFor="time">Choose Time<span aria-label="Required" className="required">*</span></label>
-            <select name="time" aria-label="Select Time" aria-required="true" id="time" onChange={handleChange} value={formData.time} required>
-              {options}
-            </select>
-          </div>
-            <div aria-label="Guest Container" className="container guestsContainer">
-            <label htmlFor="guests">Number of Guests<span aria-label="Required" className="required">*</span></label>
-            <input name="guests" aria-label="Guests Input" aria-required="true" id="guests" type="number" min="0" placeholder="1" onChange={handleChange} required />
-          </div>
-          <div aria-label="Occasion Container" className="container occasionContainer">
-            <label htmlFor="occasion">Occasion</label>
-            <select name="occasion" aria-label="Select Occasion" id="occasion" onChange={handleChange} required>
-              <option>None</option>
-              <option>Birthday</option>
-              <option>Engagement</option>
-              <option>Anniversary</option>
-            </select>
-          </div>
-            <button className="primaryBtn" text="Create Reservation" type="submit">
-            Create Reservation
-            </button>
+        <div aria-label="Date Container" className="container dateContainer">
+          <label htmlFor="date">Choose Date<span aria-label="Required" className="required">*</span></label>
+          <input name="date" aria-label="Date Input" aria-required="true" id="date" type="date" value={formData.date} onChange={handleDateChange} min={currentDate} required />
+        </div>
+        <div aria-label="Time Container" className="container timeContainer">
+          <label htmlFor="time">Choose Time<span aria-label="Required" className="required">*</span></label>
+          <select name="time" aria-label="Select Time" aria-required="true" id="time" onChange={handleInputChange} value={formData.time} required>
+            {options}
+          </select>
+        </div>
+        <div aria-label="Guest Container" className="container guestsContainer">
+          <label htmlFor="guests">Number of Guests<span aria-label="Required" className="required">*</span></label>
+          <input name="guests" aria-label="Guests Input" aria-required="true" id="guests" type="number" min="1" max="10" placeholder="1" onChange={handleInputChange} required />
+        </div>
+        <div aria-label="Occasion Container" className="container occasionContainer">
+          <label htmlFor="occasion">Occasion</label>
+          <select name="occasion" aria-label="Select Occasion" id="occasion" onChange={handleInputChange} required>
+            <option>None</option>
+            <option>Birthday</option>
+            <option>Engagement</option>
+            <option>Anniversary</option>
+          </select>
+        </div>
+        <button className="primaryBtn" text="Create Reservation" type="submit">
+          Create Reservation
+        </button>
       </form>
     </section>
-  )
+  );
 }
 
-export default BookingForm
+export default BookingForm;
--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,29 +1,35 @@
-import { useState, useReducer } from "react"
-import BookingForm from "../components/BookingForm/BookingForm"
-import { fetchAPI, submitAPI } from "../api/api"
+import { useState, useReducer } from "react";
+import BookingForm from "../components/BookingForm/BookingForm";
+import { fetchAPI, submitAPI } from "../api/api";
 import { useNavigate } from "react-router-dom";
 
 export default function BookingPage() {
-
-  // eslint-disable-next-line no-unused-vars
-  const [date, setDate] = useState(new Date())
+  const [date, setDate] = useState(new Date());
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    date: '',
+    time: '',
+    guests: '',
+    occasion: ''
+  });
 
   function initializeTimes() {
-    return fetchAPI(date)
+    return fetchAPI(date);
   }
 
   function updateTimes() {
-    const dateObj = new Date(date)
-    return fetchAPI(dateObj)
+    const dateObj = new Date(date);
+    return fetchAPI(dateObj);
   }
 
   const navigate = useNavigate();
 
-  function submitForm(formData) {
+  function submitForm() {
     const isSubmitted = submitAPI(formData);
 
     if (isSubmitted) {
-      navigate("/confirmed");
+      navigate("/confirmed", { state: { formData } });
     }
   }
 
@@ -34,16 +40,21 @@ export default function BookingPage() {
         const newDate = new Date(action.payload);
         newState = updateTimes(newDate);
         break;
-
       default:
-        throw new Error()
+        throw new Error();
     }
-    return newState
+    return newState;
   }
 
-  const [availableTimes, dispatch] = useReducer(reducer, initializeTimes())
+  const [availableTimes, dispatch] = useReducer(reducer, initializeTimes());
 
   return (
-    <BookingForm availableTimes={availableTimes} dispatch={dispatch} submitForm={submitForm}/>
-  )
+    <BookingForm
+      availableTimes={availableTimes}
+      dispatch={dispatch}
+      submitForm={submitForm}
+      formData={formData}
+      setFormData={setFormData}
+    />
+  );
 }
