15f88c08866517357592ce9e1c7509460a6c5ea9
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-06-05 10:48:56 +0200

    fix dropdown Navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 import logo from '../../assets/Logo.svg';
 import './Nav.css';
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className='nav'>
-    <Link to="/">
-      <img src={logo} alt="Little Lemon Logo" className='logo' />
-      </Link>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/menu">Menu</a></li>
-        <li><a href="/booking">Reservations</a></li>
-        <li><a href="/order">Order Online</a></li>
-        <li><a href="/login">Login</a></li>
-      </ul>
-    </nav>
+    <header>
+      <nav className='nav'>
+        <Link to="/">
+          <img src={logo} alt="Little Lemon Logo" className='logo' />
+        </Link>
+        <div className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleNav}>
+          {isOpen ? <FaTimes /> : <FaBars />}
+        </div>
+        <ul className={`nav-menu ${isOpen ? 'active' : ''}`}>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/menu">Menu</a></li>
+          <li><a href="/booking">Reservations</a></li>
+          <li><a href="/order">Order Online</a></li>
+          <li><a href="/login">Login</a></li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
