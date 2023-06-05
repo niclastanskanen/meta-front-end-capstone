4005588eb6464825104e64a621830401e212b491
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-06-05 09:13:50 +0200

    add reservation information at ConfirmedBooking page

--- src/pages/ConfirmedBooking.jsx
+++ src/pages/ConfirmedBooking.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineHome } from 'react-icons/ai';
 
 import logo_big from '../assets/logo_big.webp';
 
 const ConfirmedBooking = () => {
+  const location = useLocation();
+  const formData = location.state?.formData || {};
+
+  console.log("formData", formData);
+
+  const name = `${formData.firstName} ${formData.lastName}`;
+  const bookingInfo = `${formData.date} at ${formData.time} for ${formData.guests} guests`;
+
+  console.log("name", name);
+
   return (
     <div className='confirmedBooking'>
       <h2>Booking Confirmed!</h2>
       <img src={logo_big} alt='logo' className='confirmedLogo'/>
       <p>Your booking has been successfully confirmed.</p>
+      {bookingInfo.trim() !== '' && <p>You are booked for {bookingInfo}.</p>}
+      {name.trim() !== '' && <p>Thank you, {name}!</p>}
       <Link to='/'>
-        Go back to first page
+        Go back to the first page
         <AiOutlineHome />
       </Link>
     </div>
